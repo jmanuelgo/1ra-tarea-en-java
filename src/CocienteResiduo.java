a48cfc181b9c5daafec90c3760895427e6e58ba1
jmanuelgo <gandamanu330@gmail.com>
2021-03-22 20:52:15 -0400

    prueba subir

---
+++ src/CocienteResiduo.java
@@ -0,0 +1,21 @@
+//hallar el cociente y el residuo de una division por restas sucesivas
+import java.util.Scanner;
+public class CocienteResiduo {
+    public static void main (String args []){
+        int a,b,res=0;
+
+        Scanner read= new Scanner(System.in);
+        do{
+        System.out.println("ingrese un numero para el dividendo");
+        a=read.nextInt();
+        System.out.println("ingrese un numero para el divisor");
+        b=read.nextInt();
+        }while (a<=b && b != 0); //no detecta el 0 haciendo que no cumpla la decision ademas me gustaria que salte un mensaje que diga la razon por que la cual esta repetiendo
+        
+        while (a>=b){
+            a=a-b;
+            res=res+1;
+        }
+        System.out.println("El cociente es: "+res+ " el resto es: " +a);
+    }
+}
